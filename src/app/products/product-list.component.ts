58c510a44170b19f7a364d3e3f57f64131973d47
Patrícia Silva <prrs89@gmail.com>
2021-01-26 21:56:10 +0000

    Reading Query Parameters

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -20,32 +21,51 @@ export class ProductListComponent implements OnInit {
   }
   set listFilter(value: string) {
     this._listFilter = value;
-    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
   }
 
   filteredProducts: Product[] = [];
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.listFilter =
+      this.route.snapshot.queryParamMap.get('filterBy') ||
+      '';
+
+    this.showImage =
+      this.route.snapshot.queryParamMap.get(
+        'showImage'
+      ) === 'true';
+
     this.productService.getProducts().subscribe({
       next: products => {
         this.products = products;
-        this.filteredProducts = this.performFilter(this.listFilter);
+        this.filteredProducts = this.performFilter(
+          this.listFilter
+        );
       },
-      error: err => this.errorMessage = err
+      error: err => (this.errorMessage = err)
     });
   }
 
   performFilter(filterBy: string): Product[] {
     filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: Product) =>
-      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    return this.products.filter(
+      (product: Product) =>
+        product.productName
+          .toLocaleLowerCase()
+          .indexOf(filterBy) !== -1
+    );
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
-
 }
