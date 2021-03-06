90a13d9bb2e6a150844b5f1e9906a65faccc21d8
Patrícia Silva <prrs89@gmail.com>
2021-03-06 21:56:47 +0000

    Reading Resolver Data - Observable

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -24,12 +24,14 @@ export class ProductEditComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const resolvedData: ProductResolved = this.route
-      .snapshot.data.resolvedData;
+    this.route.data.subscribe(data => {
+      const resolvedData: ProductResolved =
+        data.resolvedData;
 
-    this.errorMessage = resolvedData.error;
+      this.errorMessage = resolvedData.error;
 
-    this.onProductRetrieved(resolvedData.product);
+      this.onProductRetrieved(resolvedData.product);
+    });
   }
 
   onProductRetrieved(product: Product): void {
