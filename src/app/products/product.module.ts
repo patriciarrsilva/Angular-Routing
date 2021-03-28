0e58d653c82f8bb46f2e00067ce8b40bcb8bbf47
Patrícia Silva <prrs89@gmail.com>
2021-03-28 18:01:18 +0100

    Component-less Routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -16,8 +16,8 @@ import { SharedModule } from '../shared/shared.module';
     RouterModule.forChild([
       {
         path: 'products',
-        component: ProductListComponent,
         children: [
+          { path: '', component: ProductListComponent },
           {
             path: ':id',
             component: ProductDetailComponent,
