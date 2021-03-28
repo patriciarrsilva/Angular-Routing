fcc316520ffba9cad6f1383b9563c52dd4639057
Patrícia Silva <prrs89@gmail.com>
2021-03-28 13:24:26 +0100

    Validating Across Child Routes

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -16,6 +16,8 @@ export class ProductEditComponent implements OnInit {
 
   product: Product;
 
+  private dataIsValid: { [key: string]: boolean } = {};
+
   constructor(
     private productService: ProductService,
     private messageService: MessageService,
@@ -73,8 +75,23 @@ export class ProductEditComponent implements OnInit {
     }
   }
 
+  isValid(path?: string): boolean {
+    this.validate();
+
+    if (path) {
+      return this.dataIsValid.path;
+    }
+
+    return (
+      this.dataIsValid &&
+      Object.keys(this.dataIsValid).every(
+        d => this.dataIsValid[d] === true
+      )
+    );
+  }
+
   saveProduct(): void {
-    if (true === true) {
+    if (this.isValid()) {
       if (this.product.id === 0) {
         this.productService
           .createProduct(this.product)
@@ -109,5 +126,31 @@ export class ProductEditComponent implements OnInit {
 
     // Navigate back to the product list
     this.router.navigate(['/products']);
+  }
+
+  validate(): void {
+    // Clear the validation object
+    this.dataIsValid = {};
+
+    // 'info' tab
+    if (
+      this.product.productName &&
+      this.product.productName.length >= 3 &&
+      this.product.productCode
+    ) {
+      this.dataIsValid.info = true;
+    } else {
+      this.dataIsValid.info = false;
+    }
+
+    // 'tags' tab
+    if (
+      this.product.category &&
+      this.product.category.length >= 3
+    ) {
+      this.dataIsValid.tags = true;
+    } else {
+      this.dataIsValid.tags = false;
+    }
   }
 }
