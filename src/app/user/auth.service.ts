d47e0f3c44ce30e630c8d34a2cef987fe1029d0a
Patrícia Silva <prrs89@gmail.com>
2021-06-26 15:27:28 +0100

    Sharing Data with a Guard

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -8,16 +8,19 @@ import { MessageService } from '../messages/message.service';
 })
 export class AuthService {
   currentUser: User;
+  redirectUrl: string;
 
   get isLoggedIn(): boolean {
     return !!this.currentUser;
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {}
 
   login(userName: string, password: string): void {
     if (!userName || !password) {
-      this.messageService.addMessage('Please enter your userName and password');
+      this.messageService.addMessage(
+        'Please enter your userName and password'
+      );
       return;
     }
     if (userName === 'admin') {
@@ -34,7 +37,9 @@ export class AuthService {
       userName,
       isAdmin: false
     };
-    this.messageService.addMessage(`User: ${this.currentUser.userName} logged in`);
+    this.messageService.addMessage(
+      `User: ${this.currentUser.userName} logged in`
+    );
   }
 
   logout(): void {
